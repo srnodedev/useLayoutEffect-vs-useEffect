92ed8d19e2e5f98ffd7e6085b2b1a6e2712c8035
seniordev <senior.node.dev@gmail.com>
2022-11-17 14:15:01 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import "./styles.css";
+import { useState, useEffect, useLayoutEffect } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("useEffect");
+  });
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect");
+  });
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
